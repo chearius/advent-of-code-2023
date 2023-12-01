88a67f7ea125d5edcffb93e7b63604ed842d4a60
Marek Šiller <marek.siller@avono.de>
2023-12-01 11:11:34 +0100

    Clean up code with spotless apply

--- src/main/kotlin/dev/siller/aoc2023/Day01.kt
+++ src/main/kotlin/dev/siller/aoc2023/Day01.kt
@@ -3,14 +3,14 @@ package dev.siller.aoc2023
 data object Day01 : AocDayTask<Int, Int>(
     day = 1,
     exampleInputPart1 =
-    """
+        """
         |1abc2
         |pqr3stu8vwx
         |a1b2c3d4e5f
         |treb7uchet
         """.trimMargin(),
     exampleInputPart2 =
-    """
+        """
         |two1nine
         |eightwothree
         |abcone2threexyz
@@ -22,18 +22,19 @@ data object Day01 : AocDayTask<Int, Int>(
     expectedExampleOutputPart1 = 142,
     expectedExampleOutputPart2 = 281
 ) {
-    private val WORDS_TO_DIGITS = mapOf(
-        "one" to '1',
-        "two" to '2',
-        "three" to '3',
-        "four" to '4',
-        "five" to '5',
-        "six" to '6',
-        "seven" to '7',
-        "eight" to '8',
-        "nine" to '9',
-        "zero" to '0'
-    )
+    private val WORDS_TO_DIGITS =
+        mapOf(
+            "one" to '1',
+            "two" to '2',
+            "three" to '3',
+            "four" to '4',
+            "five" to '5',
+            "six" to '6',
+            "seven" to '7',
+            "eight" to '8',
+            "nine" to '9',
+            "zero" to '0'
+        )
 
     override fun runPart1(input: List<String>): Int = input.sumOf(::getCalibrationValue)
 
@@ -45,7 +46,8 @@ data object Day01 : AocDayTask<Int, Int>(
         return "${digits.first()}${digits.last()}".toInt()
     }
 
-    private fun replaceWordsWithDigits(line: String): String = line.mapIndexed { index, c ->
-        WORDS_TO_DIGITS.filter { (word, _) -> line.startsWith(word, index) }.values.firstOrNull() ?: c
-    }.joinToString()
+    private fun replaceWordsWithDigits(line: String): String =
+        line.mapIndexed { index, c ->
+            WORDS_TO_DIGITS.filter { (word, _) -> line.startsWith(word, index) }.values.firstOrNull() ?: c
+        }.joinToString()
 }
