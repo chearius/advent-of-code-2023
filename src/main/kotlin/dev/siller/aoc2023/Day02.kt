cfcd8ca87dca333f00a0881ce12a20381bca255f
Marek Šiller <marek.siller@avono.de>
2023-12-01 11:23:18 +0100

    Added placeholder for Day 2

---
+++ src/main/kotlin/dev/siller/aoc2023/Day02.kt
@@ -0,0 +1,19 @@
+package dev.siller.aoc2023
+
+data object Day02 : AocDayTask<Any, Any>(
+    day = 2,
+    exampleInput =
+        """
+        |
+        """.trimMargin(),
+    expectedExampleOutputPart1 = UNKNOWN_RESULT,
+    expectedExampleOutputPart2 = UNKNOWN_RESULT
+) {
+    override fun runPart1(input: List<String>): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun runPart2(input: List<String>): Any {
+        TODO("Not yet implemented")
+    }
+}
