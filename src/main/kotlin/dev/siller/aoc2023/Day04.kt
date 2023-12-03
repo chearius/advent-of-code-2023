005c60a4f48a13095dbb49219a1ecc339f20e5c3
Marek Šiller <marek.siller@avono.de>
2023-12-03 14:22:02 +0100

    Added template for day 4 Fixed GitHub workflow

---
+++ src/main/kotlin/dev/siller/aoc2023/Day04.kt
@@ -0,0 +1,19 @@
+package dev.siller.aoc2023
+
+vcadata object Day04 : AocDayTask<Int, Int>(
+    day = 4,
+    exampleInput =
+        """
+        | 
+        """.trimMargin(),
+    expectedExampleOutputPart1 = UNKNOWN_RESULT,
+    expectedExampleOutputPart2 = UNKNOWN_RESULT
+) {
+    override fun runPart1(input: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun runPart2(input: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+}
